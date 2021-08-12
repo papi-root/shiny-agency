8697035f8a418cbe307c18b02bb367146b283300
papi-root <papi-root>
2021-08-12 19:38:34 +0000

    add Some Component and route ...

---
+++ src/pages/Survey.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function Survey() {
+    const { questionNumber } = useParams()
+
+    return (
+        <div>
+            <h1>Questionnaire !</h1>
+            <h2> Question {questionNumber}</h2>
+        </div>
+    )
+} 
+
+export default Survey 
